9709a710ac94317e116485179d6a00f30fbd60ba
Bobby Martinez <amusicalmexican@gmail.com>
2017-06-22 11:20:17 -0500

    debugging

--- goatr/commands/incident.rb
+++ goatr/commands/incident.rb
@@ -2,7 +2,7 @@ module Goatr
   module Commands
     class Incident < SlackRubyBot::Commands::Base
       command 'incident' do |client, data, _match|
-        client.say(channel: data.channel, text: 'No incidents yet, sorry.', gif: 'fire')
+        client.say(channel: data.channel, text: "No incidents yet, sorry. Here is the data #{data.channel.to_s}", gif: 'fire')
       end
     end
   end
