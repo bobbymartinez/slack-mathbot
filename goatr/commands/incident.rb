b751f629877c09e0e371decf3aa74c1042b847d0
Bobby Martinez <amusicalmexican@gmail.com>
2017-06-22 14:41:39 -0500

    added methods for incident start

--- goatr/commands/incident.rb
+++ goatr/commands/incident.rb
@@ -1,9 +1,35 @@
+require 'net/http'
+require 'uri'
 module Goatr
   module Commands
     class Incident < SlackRubyBot::Commands::Base
-      command 'incident' do |client, data, _match|
-        client.say(channel: data.channel, text: "No incidents yet, sorry. Here is the data #{data.channel.to_s}", gif: 'fire')
+
+      match(/^incident start (?<channel_name>\w*)$/i) do |client, data, match|
+        client.say(channel: data.channel, text: "Making an Incident channel with the name #{match[:channel_name]}...")
+
+        #create a channel with the first 21 characters of supplied name
+        response = create_channel(channel_name)
+
+        new_channel_id = get_channel_id(response)
+        client.say(channel: data.channel, text: "Incident channel #{channel_name} successfully created")
+
       end
+
+      class << self
+        def create_channel(channel_name)
+          Slack::Web::Client.new(token:ENV['SLACK_USER_TOKEN']).channels_create(name:channel_name[0..20])
+        end
+
+        def get_channel_id(response)
+          response['channel']['id']
+        end
+
+        def incident_name()
+
+        end
+
+      end
+
     end
   end
 end
