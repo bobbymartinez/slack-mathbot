dc8bd416b0b9e0cc8ac84a38b9fd8eb9aab8b8e0
Bobby Martinez <amusicalmexican@gmail.com>
2017-06-23 12:20:41 -0500

    added methods for inviting users to channel

--- goatr/commands/incident.rb
+++ goatr/commands/incident.rb
@@ -13,14 +13,25 @@ module Goatr
         #create a channel with the first 21 characters of supplied name
         response = create_channel(match[:channel_name])
         new_channel_id = get_channel_id(response)
-        client.say(channel: data.channel, text: "Incident channel #{match[:channel_name]} successfully created.\n http://media0.giphy.com/media/e8zJazFcehZ60/giphy.gif")
-        client.say(channel: data.channel, text: "Now starting the goat rodeo.  Inviting Ops to channel with IDS #{get_usergroup_users}")
-
+        client.say(channel: data.channel, text: "Incident channel #{match[:channel_name]} successfully created.")
+        client.say(channel: data.channel, text: "Now starting the goat rodeo.  Inviting Ops to channel.")
+        usergroup_user_ids = get_usergroup_users
+        invite_users_to_channel(new_channel_id,usergroup_user_ids)
       end
 
       class << self
         def create_channel(channel_name)
           Slack::Web::Client.new(token:@slack_user_token).channels_create(name:channel_name[0..20])
+        end
+
+        def invite_users_to_channel(channel_id,user_ids)
+          user_ids.each do |user_id|
+            invite_user_to_channel(channel_id,user_id)
+          end
+        end
+
+        def invite_user_to_channel(channel_id,user_id)
+          Slack::Web::Client.new(@slack_user_token).channels_invite(channel:channel_id,user:user_id)
         end
 
         def get_usegroups
@@ -31,7 +42,7 @@ module Goatr
           response['usergroups'].select{|usergroup| usergroup['handle'] == usergroup_handle}.first["id"]
         end
 
-        #this method isn't working for some reason. I'll look into submitting
+        #this method in the gem isn't working for some reason. I'll look into submitting
         #a patch at some point, for now, hardcoding ops user ids.
         def get_usergroup_users
           ENV['OPS_SLACK_IDS'].split(",")
